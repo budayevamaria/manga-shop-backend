d3b6f61266d0e4f338253a5e86146dea96156645
Maria <budayevamaria@gmail.com>
2026-05-11 17:22:21 +0500

    fix cors

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    origin: ["http://localhost:5173", "https://mangashop-f.vercel.app"],
     credentials: true,
   })
 );
