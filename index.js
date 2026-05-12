71d27ad7201c29bddc36149cf2008bcd210bb635
Maria <budayevamaria@gmail.com>
2026-05-12 20:58:50 +0500

    add cors

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://mangashop-f.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://mangashop-f.vercel.app",
+      "https://manga-shop-frontend.vercel.app/",
+    ],
     credentials: true,
   })
 );
